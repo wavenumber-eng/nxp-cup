bf0f7eb359b31207354b3bad9022f83ee3029cfe
nfikes <nathan@fikesfarm.com>
2024-07-22 21:52:55 -0400

    wip

--- projects/nxp-cup/src/main.c
+++ projects/nxp-cup/src/main.c
@@ -9,7 +9,16 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
 
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/device.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "ble.h"
+
+#define I2C_ADDR 0x04
+#define I2C_DATA_LEN 5
 
 LOG_MODULE_REGISTER(main);
 
@@ -48,9 +57,42 @@ static int monkey_handler(const struct shell *shell,
    return 0;
 }
 
+static int motor_drive_power_handler(const struct shell *shell,
+                                     size_t argc,
+                                     char **argv)
+{
+   ARG_UNUSED(argc);
+
+   int drive_power;
+   sscanf(argv[1], "%d", &drive_power);
+
+   int device;
+   sscanf(argv[2], "%d", &device);
+
+   const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
+    if (!device_is_ready(i2c_dev)) {
+        printk("I2C: Device is not ready.\n");
+        return -ENODEV;
+    }
+
+   uint8_t data[I2C_DATA_LEN] = {0x33, 0x00, 0x00, 0x00, 0x00};
+
+   // Perform the I2C write operation
+   int ret = i2c_write(i2c_dev, data, I2C_DATA_LEN, I2C_ADDR);
+   if (ret) {
+      printk("I2C write failed (%d)\n", ret);
+   } else {
+      printk("I2C write successful\n");
+   }
+
+   shell_fprintf(shell, SHELL_VT100_COLOR_GREEN, "Drive Power: %d\n", drive_power);
+   shell_fprintf(shell, SHELL_VT100_COLOR_GREEN, "Device: %d\n", device);
+
+   return 0;
+}
 
 SHELL_CMD_REGISTER(monkey, NULL, "magic monkey", monkey_handler);
-
+SHELL_CMD_REGISTER(motor_drive_power, NULL, "Returns hex number from decimal input for testing", motor_drive_power_handler);
 
 int main(void)
 {
@@ -58,7 +100,7 @@ int main(void)
 	LOG_INF("Hello world");
 
    ble_init();
-   
+
 	while(1)
 	{  
 		k_sleep(K_MSEC(1000));
